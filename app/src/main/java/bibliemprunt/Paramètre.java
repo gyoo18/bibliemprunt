a5af7b7db0d1711ae5f87ad8090f4d6e76579dba
Vincent Godbout <vincent_godbout@cgocable.ca>
2025-12-08 19:45:50 -0500

    Ajouté squelettes de classes

---
+++ app/src/main/java/bibliemprunt/Paramètre.java
@@ -0,0 +1,13 @@
+package bibliemprunt;
+
+public class Paramètre {
+    public static int nbMaxEmprunts;
+    public static int duréeEmpruntDéfaut;
+    public static int duréeCompteBlocage;
+    public static int authentificationEssaisMax;
+
+    public static void initialiser() {
+        // TODO implémenter initialiser
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ app/src/main/java/bibliemprunt/gui/Page.java
@@ -0,0 +1,10 @@
+package bibliemprunt.gui;
+
+import java.util.HashMap;
+
+import javax.swing.JFrame;
+
+public class Page {
+    public JFrame jFrame;
+    public HashMap<String, Object> éléments;
+}
